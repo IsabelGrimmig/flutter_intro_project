3f45a5c9828a109b78544434406e9d0eb0c7da55
IsabelGrimmig <isabel@grimmig.dk>
2024-01-16 11:39:37 +0100

    rettelser

--- lib/presentation/bottom_navigation_bar.dart
+++ lib/presentation/bottom_navigation_bar.dart
@@ -30,13 +30,10 @@ class _BottomNavigationBarWidgetState
       style: optionStyle,
     ),
     Text(
-      'Index 1: Business',
+      'Index 1: Favorites',
       style: optionStyle,
     ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
+    
   ];
 
   void _onItemTapped(int index) {
@@ -61,13 +58,10 @@ class _BottomNavigationBarWidgetState
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
+          
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: const Color.fromARGB(255, 175, 26, 76),
